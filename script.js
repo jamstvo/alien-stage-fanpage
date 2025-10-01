c43feba28266bd924b06fe41ccc4aa2e7ad5fcb0
jamstvo <guadalupeceballospinedaj@gmail.com>
2025-09-30 22:03:46 -0700

    Primer commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let lastScroll = 0;
+let showTimeout = null;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > lastScroll && currentScroll > 160) {
+    header.classList.add('hide');
+    if (showTimeout) clearTimeout(showTimeout);
+  } else {
+    if (showTimeout) clearTimeout(showTimeout);
+    showTimeout = setTimeout(() => {
+      header.classList.remove('hide');
+    }, 50); // 50ms de espera antes de mostrar el header
+  }
+  lastScroll = currentScroll;
+});
